bad9bd7fa5278a523a0fd62c5698b7bd64dedb8f
Marvin Senkiv da Silva <marvinsenkiv@brq.com>
2020-09-22 15:38:45 -0300

    xxx

--- server.js
+++ server.js
@@ -2,24 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 const cors = require('cors');
-const corsOptions = {
-    origin: 'https://cosmlov.com.br',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
+
 const app = express();
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 
-
-// starting DB
-mongoose.connect('mongodb+srv://root:root@apitestcluster-iynoc.mongodb.net/test?retryWrites=true&w=majority');
-
-requireDir('./src/models');
-// Add headers
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://cosmlov.com.br/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://cosmlov.com.br');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -34,6 +25,12 @@ app.use(function (req, res, next) {
     // Pass to next layer of middleware
     next();
 });
+// starting DB
+mongoose.connect('mongodb+srv://root:root@apitestcluster-iynoc.mongodb.net/test?retryWrites=true&w=majority');
+
+requireDir('./src/models');
+// Add headers
+
 app.use('/api', require('./src/routes'));
 var porta = process.env.PORT || 8080;
 app.listen(porta);
