ebe266d2b4bf851e103cba48e37427deaaa2a3de
Marvin Senkiv da Silva <marvinsenkiv@brq.com>
2020-09-22 15:57:57 -0300

    121313

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const routes = express.Router();
 const ProductController = require('./controller/ProductController');
+const cors = require('cors');
 
 
-
-routes.post('/callback', ProductController.worker)
+routes.post('/callback',cors(), ProductController.worker)
 module.exports = routes;
