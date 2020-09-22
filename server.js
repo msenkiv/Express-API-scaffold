38508ff909ac7740284129956059d4dcd867b2a6
Marvin Senkiv da Silva <marvinsenkiv@brq.com>
2020-09-22 15:30:59 -0300

    tes

--- server.js
+++ server.js
@@ -10,11 +10,30 @@ const app = express();
 app.use(express.json());
 app.use(cors(corsOptions));
 
+
 // starting DB
 mongoose.connect('mongodb+srv://root:root@apitestcluster-iynoc.mongodb.net/test?retryWrites=true&w=majority');
 
 requireDir('./src/models');
+// Add headers
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://cosmlov.com.br/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.use('/api', require('./src/routes'));
 var porta = process.env.PORT || 8080;
 app.listen(porta);
