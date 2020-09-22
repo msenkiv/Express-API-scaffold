95b221a4f3bbe889cfe5bf6f7d32fb84a14d9e24
Marvin Senkiv da Silva <marvinsenkiv@brq.com>
2020-09-22 15:54:52 -0300

    vfbvc

--- server.js
+++ server.js
@@ -5,26 +5,8 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
 
-app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://cosmlov.com.br');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 // starting DB
 mongoose.connect('mongodb+srv://root:root@apitestcluster-iynoc.mongodb.net/test?retryWrites=true&w=majority');
 
@@ -32,12 +14,7 @@ requireDir('./src/models');
 // Add headers
 
 
-app.all('/*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-  });
-  
+
 app.use('/api', require('./src/routes'));
 var porta = process.env.PORT || 8080;
 app.listen(porta);
