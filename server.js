eee5c9b8b2090f273e2a7dfc1476f6ce816da28a
Marvin Senkiv da Silva <marvinsenkiv@brq.com>
2020-09-29 18:42:59 -0300

    asada

--- server.js
+++ server.js
@@ -5,25 +5,24 @@ const cors = require('cors');
 
 const app = express();
 
+
+
+app.use(express.json());
+
+// starting DB
+console.log('BACON FDP')
+console.log('VAI DAIBO')
+
+requireDir('./src/models');
+// Add headers
+mongoose.connect('mongodb+srv://root:root@apitestcluster-iynoc.mongodb.net/test?retryWrites=true&w=majority');
+
+
 app.all('/*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
   });
-
-app.use(express.json());
-app.use(cors)
-app.options('*', cors());
-
-// starting DB
-console.log('BACON FDP')
-mongoose.connect('mongodb+srv://root:root@apitestcluster-iynoc.mongodb.net/test?retryWrites=true&w=majority');
-
-requireDir('./src/models');
-// Add headers
-
-
-
 app.use('/api', require('./src/routes'));
 var porta = process.env.PORT || 8080;
 app.listen(porta);
--- src/controller/ProductController.js
+++ src/controller/ProductController.js
@@ -5,17 +5,16 @@ const Callback = mongoose.model('Callback');
 
 module.exports = {
 
-
     async worker(req,res) {        
-        console.log('NEEEEEEEEEEEEEEEEEY')
+        console.log('NEEEEEEEEEEaaaqEEEEEEEY')
         let callback = {};
         console.log(req.body)
         try{
-            callback = await Callback.create(req.body);        
+            callback = await Callback.create(req.body);      
         } catch (e){
             res.send(400)
         }
-         res.json();
+        return res.send(200)
         
     }
 
